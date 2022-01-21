f4bd3f2caf2dbea2591d74b8c7b7db8578b0a75a
AS3016 <angelsilvestre3016@gmail.com>
2022-01-21 15:22:55 +0800

    Update the data in taskItemList

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -2,25 +2,33 @@ import{useState} from "react";
 import Task from "./Task";
 
 function TaskList(){
-	const taskItemsList = [
+	const [taskItemsList, setTaskItemsList] = useState([
 		"Example of Example 1",
 		"Example of Example 2",
-		"Example of Example 3",
-	];
+		"Example of Example 3"
+	]);
 
-const [taskValue, setTaskValue] = useState("Just another task");
- console.log("taskValue: ", taskValue);	
+const [taskValue, setTaskValue] = useState("");
+ // console.log("taskValue: ", taskValue);	
 
  const inputChangeHandler = (e) => {
 
  	setTaskValue(e.target.value)
  };
+
+ const addTaskHandler =() => {
+ 	setTaskItemsList([taskValue, ...taskItemsList]);
+ 	setTaskValue("");
+ };
+
 	return(
 		<div>
 			<input 
 				className="task-input" 
 				placeholder="Create a new task"
 				onChange={inputChangeHandler}
+				onBlur={addTaskHandler}
+				value={taskValue}
 			/>
 			<ul>
 				{taskItemsList.map((task, index) => {
